9e1377a545650ceebd89b484df3064e5f97928e5
user <1296800094@qq.com>
2024-05-17 10:36:00 +0800

    feat:添加令牌桶算法测试类

--- byapi-server/src/test/java/com/example/ByapiServerApplicationTests.java
+++ byapi-server/src/test/java/com/example/ByapiServerApplicationTests.java
@@ -1,21 +1,84 @@
 package com.example;
 
 import com.example.client.ByApiClient;
+import com.example.common.utils.TokenBucketLimiter;
 import com.example.server.ByapiServerApplication;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootTest(classes = {ByapiServerApplication.class})
+@Slf4j
 class ByapiServerApplicationTests {
 
     @Resource
     private ByApiClient byApiClient;
+    @Resource
+    private TokenBucketLimiter bucketLimiter;
+
+    //线程池，用于多线程模拟测试
+    private ExecutorService pool = Executors.newFixedThreadPool(10);
 
     @Test
     void contextLoads() {
         System.out.println(byApiClient.getName("张三"));
     }
 
+    @Test
+    void testLimited() {
+        // 被限制的次数
+        AtomicInteger limited = new AtomicInteger(0);
+        // 线程数
+        final int threads = 2;
+        // 每条线程的执行轮数
+        final int turns = 20;
+
+
+        // 同步器
+        CountDownLatch countDownLatch = new CountDownLatch(threads);
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < threads; i++) {
+            pool.submit(() ->
+            {
+                try {
+
+                    for (int j = 0; j < turns; j++) {
+
+                        boolean intercepted = bucketLimiter.isLimited();
+                        if (intercepted) {
+                            // 被限制的次数累积
+                            limited.getAndIncrement();
+                        }
+
+                        Thread.sleep(200);
+                    }
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                //等待所有线程结束
+                countDownLatch.countDown();
+
+            });
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        float time = (System.currentTimeMillis() - start) / 1000F;
+        //输出统计结果
+
+        log.info("限制的次数为：" + limited.get() +
+                ",通过的次数为：" + (threads * turns - limited.get()));
+        log.info("限制的比例为：" + (float) limited.get() / (float) (threads * turns));
+        log.info("运行的时长为：" + time);
+    }
 }
